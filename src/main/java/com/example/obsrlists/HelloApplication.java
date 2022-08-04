fa0af11c7333020084584c8fb7234478ac1a8b63
James Keogh <jkeogh@adga.ca>
2022-08-04 13:37:07 -0400

    adds a working button

--- src/main/java/com/example/obsrlists/HelloApplication.java
+++ src/main/java/com/example/obsrlists/HelloApplication.java
@@ -12,9 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(
-            HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+            HelloApplication.class.getResource("observerList.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("ObserverLists");
         stage.setScene(scene);
         stage.show();
     }
